733f51468ce91494fd2745b0b16744ac7c1ef2b4
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-05-27 17:28:36 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function populateVideo() {
+  const savedTime = localStorage.setItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+  populateVideo();
+}
